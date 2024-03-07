30b6405e106b3fb617fcf1d12e36171cfdaf72fe
John Miyao <jjmiyao@gmail.com>
2024-03-06 16:05:18 -0800

    Added JS for share pop-up menu

--- challenges/newbie-js/article-preview-component/index.js
+++ challenges/newbie-js/article-preview-component/index.js
@@ -1,22 +1,38 @@
 const shareButton = document.querySelector('.share');
 const $shareMenu = document.querySelector('.share-menu');
 
+// function shareMenu() {
+// 	event.preventDefault();
+// 	console.log('click');
+
+// 	$shareMenu.style.opacity = '1';
+// 	$shareMenu.style.visibility = 'visible';
+// 	$shareMenu.style.display = 'flex';
+// }
+
+
+
+
 function shareMenu() {
+  const $shareMenu = document.querySelector('.share-menu'); // Replace 'yourShareMenuId' with the actual ID of your share menu
+
+  if ($shareMenu.style.opacity === '1') {
+    // If the menu is currently visible, hide it
+    $shareMenu.style.opacity = '0';
+    $shareMenu.style.visibility = 'hidden';
+    $shareMenu.style.display = 'none';
+  } else {
+    // If the menu is currently hidden, show it
+    $shareMenu.style.opacity = '1';
+    $shareMenu.style.visibility = 'visible';
+    $shareMenu.style.display = 'flex';
+  }
+}
+
+function hideMenu() {
 	event.preventDefault();
 	console.log('click');
 
-	$shareMenu.style.opacity = '1';
-	$shareMenu.style.visibility = 'visible';
-	$shareMenu.style.display = 'flex';
+	$shareMenu.style.opacity = '0';
+	$shareMenu.style.visibility = 'hidden';
 }
-
-
-
-$articleCard = document.querySelector('.share-menu');
-
-$articleCard.addEventListener('click', function() {
-	if ($shareMenu.style.opacity === '1') {
-		$shareMenu.style.opacity = '0';
-		$shareMenu.style.visibility = 'hidden';
-	} 
-})
