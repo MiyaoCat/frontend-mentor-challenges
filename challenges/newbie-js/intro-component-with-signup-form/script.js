07dd30509a77edf144d9dfe65bdddb1508e5e58a
jmiyao <jjmiyao@gmail.com>
2024-03-15 14:33:49 -0700

    Grouped inputs, added email javascript function

--- challenges/newbie-js/intro-component-with-signup-form/script.js
+++ challenges/newbie-js/intro-component-with-signup-form/script.js
@@ -1,6 +1,19 @@
+$form = document.querySelector('form');
 $button = document.querySelector('button');
-$error = document.querySelector('.error');
-$button.addEventListener('click', function(event) {
+$emailError = document.querySelector('.email');
+
+$form.addEventListener('click', function(event) {
 	event.preventDefault();
-	console.log('click');
+	var $input = document.querySelector("input[type='email']");
+	const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  const emailInput = $input.value;
+
+  const trimmedEmail = emailInput.trim(); 
+
+  if (emailInput === '' || trimmedEmail && !emailRegex.test(trimmedEmail)) {
+    $emailError.style.display = 'block';
+    $input.style.border = '1px solid red';
+  } else {
+    $emailError.style.display = 'none';
+  }
 })
