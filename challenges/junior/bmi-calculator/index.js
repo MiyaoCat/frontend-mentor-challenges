cff2db74fdc75935ae357371da3a2ff474a1a184
John Miyao <jjmiyao@gmail.com>
2024-04-12 17:43:04 -0700

    Added BMI calculations, prevented NaN output

--- challenges/junior/bmi-calculator/index.js
+++ challenges/junior/bmi-calculator/index.js
@@ -3,12 +3,14 @@ const $measurements = document.querySelector('.measurements');
 const $heightWrap = document.querySelector('.height');
 
 const $imperialType = document.querySelector('#imperial');
+const $metricType = document.querySelector('#metric');
 
 const $heightImperial = document.querySelector('.height-imperial');
 const $heightMetric = document.querySelector("label[for='heightMetric']");
 
-const weightLabel = document.querySelector('.kg');
+const $weightLabel = document.querySelector('.kg');
 
+const $bmi = document.querySelector('.bmi');
 // BMI CALCULATION
 const heightCm = document.querySelector('#heightMetric');
 const heightFeet = document.querySelector('#heightFt');
@@ -25,41 +27,33 @@ let weightLbValue;
 
 heightCm.addEventListener('change', function() {
 	heightCmValue = parseInt(this.value);
-	calculateBMI()
+	calculateBMI();
 })
 
 heightFeet.addEventListener('change', function() {
 	heightFtValue = parseInt(this.value);
-	console.log(heightFtValue);
+	calculateBMI();
 })
 
 heightIn.addEventListener('change', function() {
 	heightInValue = parseInt(this.value);
-	console.log(heightInValue);
+	calculateBMI();
 })
 
 weightKg.addEventListener('change', function() {
 	weightKgValue = parseInt(this.value);
-	calculateBMI()
-})
-
-console.log('weight in kg: ', weightKgValue)
-
-
-
-document.addEventListener('keydown', function(event) {
-	if (event.key === 'Enter') {
-		console.log('metric bmi: ', metricBMI);
-		console.log('weight in kg: ', weightKgValue)
-	}
+	calculateBMI();
 })
 
 let metricBMI;
+let imperialBMI;
 
-$unitType.addEventListener('change', function() {
+$unitType.addEventListener('input', function() {
 
 	if ($imperialType.checked) {
-
+		
+		calculateBMI();
+	
 		if (window.innerWidth <= 678) {
 			$measurements.style.gridTemplateRows = '1fr 1fr';
 			$heightWrap.style.gridColumn = '1 / 3';
@@ -68,32 +62,60 @@ $unitType.addEventListener('change', function() {
 		$heightImperial.style.display = 'flex';
 		$heightMetric.style.display = 'none';
 
-		weightLabel.innerText = 'lb';
+		$weightLabel.innerText = 'lb';
 
-	} else {
+	} 
 
-		metricBMI = weightKgValue / (heightCm * heightCm);
+	if ($metricType.checked) {
 
+		calculateBMI();
+		
 		if (window.innerWidth <=650) {
 			$measurements.style.gridTemplateRows = '1fr';
 			$heightWrap.style.gridColumn = '1 / 2';
 		}
 
 		$heightImperial.style.display = 'none';
-		$heightMetric.style.display = 'flex';
+		$heightMetric.style.display = 'flex';		
 
-		weightLabel.innerText = 'kg';
+		$weightLabel.innerText = 'kg';
 	}
 })
 
 
 function calculateBMI() {
-	if (heightCmValue !== '' || heightCmValue !== undefined && weightKgValue !== '' || weightKgValue !== undefined) {
-		const heightMeters = heightCmValue / 100;
-		metricBMI = ( weightKgValue / (heightMeters * heightMeters) ).toFixed(1);
 
-		console.log('metric bmi: ', metricBMI);
+	if ($metricType.checked) {
+		if (heightCmValue !== '' || heightCmValue !== undefined && weightKgValue !== '' || weightKgValue !== undefined) {
+
+			const heightMeters = heightCmValue / 100;
+
+			metricBMI = ( weightKgValue / (heightMeters * heightMeters) ).toFixed(1);
+
+			if ( !isNaN(metricBMI) ) {
+				$bmi.innerHTML = metricBMI;
+			} else {
+				$bmi.innerHTML = '';
+			}
+		}
+	}	
+
+	if ($imperialType.checked) {
+		if (heightFtValue !== '' || heightFtValue !== undefined && heightInValue !== '' || heightInValue !== undefined && weightLbValue !== '' || weightLbValue !== undefined) {
+
+			const heightInInches = heightFtValue * 12 + heightInValue;
+
+			imperialBMI = ( (weightKgValue * 703) / (heightInInches * heightInInches) ).toFixed(1);
+
+			if ( !isNaN(imperialBMI) ) {
+				$bmi.innerHTML = imperialBMI;
+			} else {
+				$bmi.innerHTML = '';
+			}
+
+		}
 	}
+		
 }
 
 
