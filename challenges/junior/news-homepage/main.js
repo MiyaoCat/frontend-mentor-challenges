383137c3e81750f1eda82da69fda6a9f8059e8f3
John Miyao <jjmiyao@gmail.com>
2024-05-06 16:58:02 -0700

    Re-did header toggle

--- challenges/junior/news-homepage/main.js
+++ challenges/junior/news-homepage/main.js
@@ -1,20 +1,18 @@
 const menuButton = document.querySelector('.hamburger-menu');
 const menuExitButton = document.querySelector('.exit-menu-btn');
-const menu = document.querySelector('.menu');
+const menu = document.querySelector('.site-menu');
 
 function toggleMenu() {
-	if (menu.style.marginLeft ==='500px') {
-		// menu.style.display = 'block';
-		menu.style.marginLeft = '72px';
-		menuButton.style.display = 'none';
-		menuExitButton.style.display = 'block';
-		
+
+	menu.style.display('none')
+	if (menu.style.display === 'none') {
+		menu.style.display = menu.style.display === 'none' ? 'flex' : 'none';
 	} else {
-		menu.style.marginLeft = '500px';
-		menuButton.style.display = 'block';
-		menuExitButton.style.display = 'none';
+		menu.style.display = 'none'
 	}
+
 }
+
 
 console.log('menu')
 console.log(menu);
