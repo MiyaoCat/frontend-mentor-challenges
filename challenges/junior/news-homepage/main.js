fb5c9e063de8c9cc6191dd1c639e67a22042982b
John Miyao <jjmiyao@gmail.com>
2024-05-07 15:56:34 -0700

    Setup nav button function

--- challenges/junior/news-homepage/main.js
+++ challenges/junior/news-homepage/main.js
@@ -1,18 +1,13 @@
-const menuButton = document.querySelector('.hamburger-menu');
-const menuExitButton = document.querySelector('.exit-menu-btn');
+const menuButtonToggle = document.querySelector('.menu-button-toggle');
 const menu = document.querySelector('.site-menu');
+const body = document.querySelector('body');
 
 function toggleMenu() {
+	var computedDisplay = window.getComputedStyle(menu).getPropertyValue('display');
 
-	menu.style.display('none')
-	if (menu.style.display === 'none') {
-		menu.style.display = menu.style.display === 'none' ? 'flex' : 'none';
-	} else {
-		menu.style.display = 'none'
-	}
+	menu.style.display = computedDisplay === 'none' ? 'flex' : 'none';
 
+	menuButtonToggle.toggleAttribute('data-closeButton');
+
+	body.toggleAttribute('data-overlay');
 }
-
-
-console.log('menu')
-console.log(menu);
