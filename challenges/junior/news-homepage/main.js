96adf63b4dcd546366546f9b913dce44c80ef6c7
John Miyao <jjmiyao@gmail.com>
2024-05-23 17:15:41 -0700

    Styled desktop view

--- challenges/junior/news-homepage/main.js
+++ challenges/junior/news-homepage/main.js
@@ -3,10 +3,6 @@ const menu = document.querySelector('.menu-wrap');
 const body = document.querySelector('body');
 
 const toggle = function toggleMenu() {
-	// var computedDisplay = window.getComputedStyle(menu).getPropertyValue('display');
-
-	// menu.style.display = computedDisplay === 'none' ? 'block' : 'none';
-	// menu.hasAttribute('data-show') ? menuButtonToggle.setAttribute('data-closebutton', false) : menuButtonToggle.setAttribute('data-show', true);
 
 	menu.toggleAttribute('data-show');
 	menuButtonToggle.toggleAttribute('data-closebutton');
